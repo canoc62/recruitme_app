910d7674d93583ea21e511eb42ec7c76438be234
Christopher Cano <canoc4262@gmail.com>
2015-11-18 19:42:25 -0800

    Add same_profile? method.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,8 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar")
   end
+
+  def same_profile?
+    current_user.id == params[:id].to_i
+  end
 end
