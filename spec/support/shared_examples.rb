14f153f525af6aee926862fe5d5d01cefa5335c4
Christopher Cano <canoc4262@gmail.com>
2015-12-07 04:47:51 -0800

    Take out sign in shared example method.

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,11 +1,3 @@
-shared_examples "requires sign in" do
-  it "redirects to the sign in page" do
-    clear_current_user
-    action
-    expect(response).to redirect_to sign_in_path
-  end 
-end
-
 shared_examples "show error and go to root" do
   it "shows the flash error message" do
     expect(flash[:error]).not_to be_empty
